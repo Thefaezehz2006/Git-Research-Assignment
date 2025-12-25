fc9a2223a750fddad76acb51bb1180d01a86d53b
Thefaezehz2006 <faezehzeydabadi077@gmail.com>
2025-12-25 21:54:59 +0330

    Create fork.mk

---
+++ fork.mk
@@ -0,0 +1,3 @@
+Fork means creating a copy of someone else's repository into your own GitHub account.
+It allows you to freely experiment with changes without affecting the original project.
+Forking is commonly used in open-source projects.
