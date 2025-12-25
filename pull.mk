1a9b60e026f1a16327e49fa3a3ea78262b925c1a
Thefaezehz2006 <faezehzeydabadi077@gmail.com>
2025-12-25 22:04:42 +0330

    Create pull.mk

---
+++ pull.mk
@@ -0,0 +1,2 @@
+Pull means fetching the latest changes from a remote repository (like origin) and merging them into your current local branch. The git pull command is essentially git fetch (download changes) + git merge. 
+It is used to keep your local code up to date with others' changes.
