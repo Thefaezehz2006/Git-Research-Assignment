72412d70ef7d3e7293b03c9883f18baeef2df2e0
Thefaezehz2006 <faezehzeydabadi077@gmail.com>
2025-12-25 21:58:00 +0330

    Update fork.mk

--- fork.mk
+++ fork.mk
@@ -1,3 +1,3 @@
-Fork means creating a copy of someone else's repository into your own GitHub account.
-It allows you to freely experiment with changes without affecting the original project.
-Forking is commonly used in open-source projects.
+A fork is creating a complete and independent copy of someone else's repository under your own GitHub account. 
+It is commonly used to contribute to open-source projects when you don't have direct write access.
+The fork copies all files and commit history, but not issues or previous pull requests. You can then make changes and propose them back via a Pull Request.
